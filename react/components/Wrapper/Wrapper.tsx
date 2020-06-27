7d771ea32794e5823494c2d1b2d73fd263b7e724
Bruno Moreira <bruno@brunormoreira.com>
2020-06-27 14:44:49 -0300

    Add query to MD

--- react/components/Wrapper/Wrapper.tsx
+++ react/components/Wrapper/Wrapper.tsx
@@ -1,27 +1,52 @@
-import React, { FC, Fragment } from 'react'
+import React, { FC, Fragment, useEffect } from 'react'
 import { LoadingContextProvider } from 'vtex.render-runtime'
-import product from 'vtex.store-resources/QueryProduct'
 import ProductContextProvider from 'vtex.product-context/ProductContextProvider'
 import { useQuery } from 'react-apollo'
+import PRODUCT_QUERY from 'vtex.store-resources/QueryProduct'
+
+import DOCUMENTS_QUERY from '../../graphql/documents.graphql'
 
 const SLUG = 'daily-pack'
+const ACRONYM = 'dailypack'
+const FIELDS = ['country', 'element', 'dailyDosage']
+const SCHEMA = 'v1'
+
 const Wrapper: FC<{ query: Record<string, string> }> = ({
   query,
   children,
 }) => {
-  const { data, loading } = useQuery(product, {
-    variables: {
-      slug: SLUG,
-      skipCategoryTree: true,
-      identifier: { field: 'id', value: '' },
-    },
-    errorPolicy: 'all',
-  })
+  const { data: productData, loading: productLoading } = useQuery(
+    PRODUCT_QUERY,
+    {
+      variables: {
+        slug: SLUG,
+        skipCategoryTree: true,
+        identifier: { field: 'id', value: '' },
+      },
+      errorPolicy: 'all',
+    }
+  )
+
+  const { data: documentsData, loading: documentsLoading } = useQuery(
+    DOCUMENTS_QUERY,
+    {
+      variables: {
+        acronym: ACRONYM,
+        schema: SCHEMA,
+        fields: FIELDS,
+      },
+    }
+  )
+
+  useEffect(() => {
+    // eslint-disable-next-line no-console
+    console.log(documentsData)
+  }, [documentsData])
 
   return (
     <Fragment>
-      <ProductContextProvider query={query} product={data?.product}>
-        <LoadingContextProvider value={loading}>
+      <ProductContextProvider query={query} product={productData?.product}>
+        <LoadingContextProvider value={productLoading && documentsLoading}>
           {children}
         </LoadingContextProvider>
       </ProductContextProvider>
