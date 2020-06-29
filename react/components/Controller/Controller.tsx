c00a0372ac589473cb7c341565ddea74cce3300e
Bruno Moreira <bruno@brunormoreira.com>
2020-06-29 14:23:54 -0300

    Add control for add-to-cart-button

--- react/components/Controller/Controller.tsx
+++ react/components/Controller/Controller.tsx
@@ -1,15 +1,19 @@
 import React, { FC, Fragment, useCallback, useMemo } from 'react'
+import { useChildBlock, ExtensionPoint } from 'vtex.render-runtime'
 import useProduct from 'vtex.product-context/useProduct'
 
-// import { useDailyPack } from '../../context/DailyPackContext'
+import { useDailyPack } from '../../context/DailyPackContext'
 
 const DOSAGE = 'DOSAGE'
 const ELEMENT = 'ELEMENT'
-// const COUNTRY = 'CAN'
+const COUNTRY = 'CAN'
 
-const Controller: FC = ({ children }) => {
+const Controller: FC = () => {
   const { product } = useProduct()
-  // const { table } = useDailyPack()
+  const { table } = useDailyPack()
+  const hasButton = useChildBlock({
+    id: 'add-to-cart-button',
+  })
 
   const getPropertyValue = useCallback(
     propertyName => {
@@ -25,17 +29,38 @@ const Controller: FC = ({ children }) => {
     [product]
   )
 
-  getPropertyValue(DOSAGE)
-
   const dosage = useMemo(() => getPropertyValue(DOSAGE), [getPropertyValue])
-
   const element = useMemo(() => getPropertyValue(ELEMENT), [getPropertyValue])
+
+  const dailyDosage = useMemo(() => {
+    if (!element) {
+      return null
+    }
+    const data = table.find(
+      value =>
+        value.country === COUNTRY &&
+        value.element.toUpperCase() === element?.toUpperCase()
+    )
+
+    return data?.dailyDosage
+  }, [table, element])
+
+  const allowed = useMemo(() => {
+    if (Number.isNaN(dailyDosage as any) || Number.isNaN(dosage as any)) {
+      return true
+    }
+
+    return Number(dosage) <= Number(dailyDosage)
+  }, [dosage, dailyDosage])
 
   return (
     <Fragment>
-      <div>{dosage}</div>
-      <div>{element}</div>
-      <div>{children}</div>
+      <div>Dosage: {dosage ?? 'not specified'}</div>
+      <div>Element: {element ?? 'not specified'}</div>
+      <div>Daily Dosage Allowed: {dailyDosage ?? 'not specified'}</div>
+      {hasButton ? (
+        <ExtensionPoint id="add-to-cart-button" disabled={!allowed} />
+      ) : null}
     </Fragment>
   )
 }
--- react/typings/vtex.render-runtime.d.ts
+++ react/typings/vtex.render-runtime.d.ts
@@ -1,5 +1,16 @@
+import { ComponentType } from 'react'
+import { ExtensionPointProps } from 'vtex.render-runtime'
+
 declare module 'vtex.render-runtime' {
   import { FC } from 'react'
 
+  export const ExtensionPoint: ComponentType<ExtensionPointProps>
+
+  interface ChildBlockProps {
+    id: string
+  }
+
+  export const ChildBlock: ComponentType<ChildBlockProps>
+  export const useChildBlock = function ({ id: string }): object {}
   export const LoadingContextProvider: FC<{ value: boolean }>
 }
